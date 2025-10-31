bd621fa2cdefd9f8417484396cb6a96fcc17dbab
Your Name <minajmiha02@gmail.com>
2025-10-31 21:21:51 +0600

    first commit

---
+++ src/routes/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../Pages/Home/Home";
+const  router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App></App>,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+        ],
+    },
+]);
+export default router;
