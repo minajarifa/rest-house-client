b89ad4cb28217a3edd5db644e092ce92d524f759
Your Name <minajmiha02@gmail.com>
2025-11-28 17:50:05 +0600

    add

--- src/Components/Categories/CategoryBox.jsx
+++ src/Components/Categories/CategoryBox.jsx
@@ -1,26 +1,27 @@
-import PropTypes from 'prop-types'
-import queryString from 'query-string'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import PropTypes from "prop-types";
+import queryString from "query-string";
+import { useNavigate, useSearchParams } from "react-router-dom";
 const CategoryBox = ({ label, icon: Icon }) => {
   // eslint-disable-next-line no-unused-vars
-  const [params, setParams] = useSearchParams()
-  const category = params.get('category')
-
-  const navigate = useNavigate()
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+  const navigate = useNavigate();
   const handleClick = () => {
     // 1. Create Query String
-    let currentQuery = { category: label }
+    let currentQuery = { category: label };
     const url = queryString.stringifyUrl({
-      url: '/',
+      url: "/",
       query: currentQuery,
-    })
+    });
+    console.log(url);
     // url output---> /?category=label
     // 2. Set query string in url
-    navigate(url)
-  }
+    navigate(url);
+  };
   return (
     <div
       onClick={handleClick}
+      // onClick={()=>console.log(label)}
       className={`flex 
     flex-col 
     items-center 
@@ -31,16 +32,16 @@ const CategoryBox = ({ label, icon: Icon }) => {
    hover:text-neutral-800
     transition
     cursor-pointer  ${
-      category === label && 'border-b-neutral-800 text-neutral-800'
+      category === label && "border-b-neutral-800 text-neutral-800"
     } `}
     >
       <Icon size={26} />
-      <div className='text-sm font-medium'>{label}</div>
+      <div className="text-sm font-medium">{label}</div>
     </div>
-  )
-}
+  );
+};
 CategoryBox.propTypes = {
   label: PropTypes.string,
   icon: PropTypes.elementType,
-}
-export default CategoryBox
+};
+export default CategoryBox;
