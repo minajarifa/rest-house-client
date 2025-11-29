1f50e05138ec2902799f63782ae48d9632d2acfb
Your Name <minajmiha02@gmail.com>
2025-11-29 17:10:28 +0600

    file

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,24 +1,23 @@
 import { FcGoogle } from "react-icons/fc";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 export default function Login() {
+   const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    
+  };
   return (
     <div>
       <section className="bg-white dark:bg-gray-900">
         <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
-          <form className="w-full max-w-md">
-            <img
-              className="w-auto h-7 sm:h-8"
-              src="https://merakiui.com/images/logo.svg"
-              alt=""
-            />
-            <h1 className="mt-3 text-2xl font-semibold text-gray-800 capitalize sm:text-3xl dark:text-white">
+          <form onSubmit={handleLoginSubmit} className="w-full max-w-md">
+            <h1 className="mt-3 text-2xl font-semibold text-gray-800 capitalize sm:text-3xl dark:text-white text-center">
               sign In
             </h1>
-
             <div className="relative flex items-center mt-8">
               <span className="absolute ">
-               <MdOutlineEmail className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"/>
+                <MdOutlineEmail className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" />
               </span>
               <input
                 type="email"
@@ -28,7 +27,7 @@ export default function Login() {
             </div>
             <div className="relative flex items-center mt-4">
               <span className="absolute">
-               <RiLockPasswordFill className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"/>
+                <RiLockPasswordFill className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" />
               </span>
               <input
                 type="password"
@@ -36,32 +35,30 @@ export default function Login() {
                 placeholder="Password"
               />
             </div>
-
             <div className="mt-6">
               <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                 Sign in
               </button>
-
               <p className="mt-4 text-center text-gray-600 dark:text-gray-400">
                 or sign in with
               </p>
-
               <a
                 href="#"
                 className="flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
               >
-               <FcGoogle className="text-3xl" />
-
+                <FcGoogle className="text-3xl" />
                 <span className="mx-2">Sign in with Google</span>
               </a>
-
               <div className="mt-6 text-center ">
-                <a
-                  href="#"
-                  className="text-sm text-blue-500 hover:underline dark:text-blue-400"
-                >
-                  Don’t have an account yet? Sign up
-                </a>
+                <p className="text-sm text-blue-500  ">
+                  Don’t have an account yet?
+                  <Link
+                    to="/Register"
+                    className="dark:text-blue-400 hover:underline"
+                  >
+                    Sign up
+                  </Link>
+                </p>
               </div>
             </div>
           </form>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,29 +1,25 @@
 import { CiUser } from "react-icons/ci";
+import { FcGoogle } from "react-icons/fc";
 import { GoUpload } from "react-icons/go";
 import { HiOutlineMail } from "react-icons/hi";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
 export default function Register() {
+  const handleRegister = (e) => {
+    e.preventDefault();
+   const form = e.target;
+   console.log(form)
+  };
   return (
     <div>
       <section className="bg-white dark:bg-gray-900">
         <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
-          <form className="w-full max-w-md">
-            <div className="flex justify-center mx-auto">
-              <img
-                className="w-auto h-7 sm:h-8"
-                src="https://merakiui.com/images/logo.svg"
-                alt=""
-              />
-            </div>
+          <form onSubmit={handleRegister} className="w-full max-w-md">
             <div className="flex items-center justify-center mt-6">
-              <a
-                href="#"
-                className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400 dark:text-white"
-              >
+              <p className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400 dark:text-white">
                 sign up
-              </a>
+              </p>
             </div>
             <div className="relative flex items-center mt-8">
               <span className="absolute">
@@ -31,8 +27,10 @@ export default function Register() {
               </span>
               <input
                 type="text"
-                className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 placeholder="Username"
+                name="name"
+                required
+                className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
             <label
@@ -41,7 +39,7 @@ export default function Register() {
             >
               <GoUpload className="w-6 h-6 text-gray-300 dark:text-gray-500" />
               <h2 className="mx-3 text-gray-400">Profile Photo</h2>
-              <input id="dropzone-file" type="file" className="hidden" />
+              <input  id="image" type="file" className="hidden" name="" accept="image/*"/>
             </label>
             <div className="relative flex items-center mt-6">
               <span className="absolute">
@@ -49,6 +47,8 @@ export default function Register() {
               </span>
               <input
                 type="email"
+                required
+                name="email"
                 className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 placeholder="Email address"
               />
@@ -59,6 +59,8 @@ export default function Register() {
               </span>
               <input
                 type="password"
+                required
+                name="password"
                 className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 placeholder="Password"
               />
@@ -69,18 +71,36 @@ export default function Register() {
               </span>
               <input
                 type="password"
+                required
+                name="confirmPassword"
+                
                 className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 placeholder="Confirm Password"
               />
             </div>
             <div className="mt-6">
               <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                Sign Up
+                Continue
               </button>
+              {/*  */}
+              <p className="mt-4 text-center text-gray-600 dark:text-gray-400">
+                or sign in with
+              </p>
+              <a
+                href="#"
+                className="flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <FcGoogle className="text-3xl" />
+                <span className="mx-2">Sign in with Google</span>
+              </a>
+              {/*  */}
               <div className="mt-6 text-center ">
                 <p className="text-sm ">
                   Already have an account? plaese
-                  <Link className="mx-5 text-blue-500 hover:underline dark:text-blue-400">
+                  <Link
+                    to="/Login"
+                    className="mx-5 text-blue-500 hover:underline dark:text-blue-400"
+                  >
                     Login
                   </Link>
                 </p>
